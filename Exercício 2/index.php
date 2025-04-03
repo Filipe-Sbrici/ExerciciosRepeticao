6dfbb446c0d62fff03926254df0f2c683c612fab
Caua-Alborghette <cauaalborghette09@gmail.com>
2025-04-03 07:44:28 -0300

    C 04

--- Exercício 2/index.php
+++ Exercício 2/index.php
@@ -4,16 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 2</title>
+    <link href="styles.css" rel="stylesheet" >
 </head>
 <body>
-    <link href="styles.css" rel="stylesheet" >
     <?php
         
-        for ($i = 0; $i == 10; $i++) {
-            echo " $i ";
+        for ($i = 0; $i < 10; $i++) {
+            echo " $i <br>";
         }
 
-
-        /?>
+    ?>
 </body>
 </html>
