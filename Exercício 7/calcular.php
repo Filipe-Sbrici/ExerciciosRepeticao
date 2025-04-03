208247a2f232432e9ecec2477a6130c594c3bf36
Filipe-Sbrici <filipesbrici@gmail.com>
2025-04-03 18:42:29 -0300

    f 0.8

---
+++ Exercício 7/calcular.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5</title>
+    <link href="styles.css" rel="stylesheet" >
+</head>
+<body>
+    <?php
+        $num = $_POST["var"];
+    
+        for ($i <= $num; $i <= 0; $i+){
+            if ($i % 2 == 0){
+                echo " $i <br>";
+            }
+        }
+    ?>
+</body>
+</html>
---
+++ Exercício 7/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 7</title>
+    <link href="styles.css" rel="stylesheet" >
+</head>
+<body>
+    <form action="calcular.php" method="post">
+    <h2>Insira um número, para que sejam mostrados todos os pares entre ele e 0:<input type="number" name="var"></h2>
+    <input type="submit" text = "enviar">
+    </form>
+</body>
+</html>
