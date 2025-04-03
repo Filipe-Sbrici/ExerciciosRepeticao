4010b1dbb8f48a8a6ae1685f082bc9619a1d49b2
Caua-Alborghette <cauaalborghette09@gmail.com>
2025-04-03 08:32:55 -0300

    C 15

---
+++ Execício 10/calcular.php
@@ -0,0 +1,10 @@
+<?php
+
+$num = $_POST ["num"];
+
+for ($i = num; $i <= 0; $i--) {
+    
+    echo "$i<br>";
+}
+
+?>
---
+++ Execício 10/indes.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+</head>
+<body>
+<form method = "post" action="calcular.php">
+    <h2>Insira o número</h2>
+    <input type = "text"  name="frase" size="25" /> <br></br>
+    <input type = "submit" name="Enviadar" size="25">
+</form>
+</body>
+</html>
