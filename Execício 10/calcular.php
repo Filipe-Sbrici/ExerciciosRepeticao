4d0297ea674350a0a83907f4df2c9e2366d777ef
Filipe-Sbrici <filipesbrici@gmail.com>
2025-04-03 19:13:30 -0300

    f 1.4

--- Execício 10/calcular.php
+++ Execício 10/calcular.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+    <link href="styles.css" rel="stylesheet" >
+</head>
+<body>
 <?php
 
 $num = $_POST ["num"];
@@ -8,4 +17,5 @@ for ($i = $num; $i >= 0; $i -= $inter) {
 }
 
 ?>
-
+</body>
+</html>
--- Execício 10/index.php
+++ Execício 10/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 10</title>
+    <link href="styles.css" rel="stylesheet" >
 </head>
 <body>
 <form method = "post" action="calcular.php">
